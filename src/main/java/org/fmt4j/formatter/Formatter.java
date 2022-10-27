e23d5ff0557ed3c3271124b9d9b4476b63b34192
linrongbin16 <linrongbin16@outlook.com>
2022-10-27 14:49:40 +0800

    add junit sample case

--- src/main/java/org/fmt4j/formatter/Formatter.java
+++ src/main/java/org/fmt4j/formatter/Formatter.java
@@ -1,4 +1,15 @@
 package org.fmt4j.formatter;
 
 public class Formatter {
+    public static String format() {
+        return null;
+    }
+
+    public static String println() {
+        return null;
+    }
+
+    public static String print() {
+        return null;
+    }
 }
--- src/test/java/org/fmt4j/formatter/TestFormatter.java
+++ src/test/java/org/fmt4j/formatter/TestFormatter.java
@@ -1,4 +1,12 @@
 package org.fmt4j.formatter;
 
+import org.junit.Test;
+
 public class TestFormatter {
+    @Test
+    public void test() {
+        Formatter.format();
+        Formatter.print();
+        Formatter.println();
+    }
 }
